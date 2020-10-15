32e373686a596dc059e09c69a2c29fc74d0d34e3
LuizRoyer <32273858+LuizRoyer@users.noreply.github.com>
2020-10-15 18:09:00 -0300

    add backend Node com banco de Dados MongoDB

---
+++ backend-scm/config/keys-dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    secretOrKey: "CursoAlura",
+    clientId: "ceffbcb3d82963e5efe8",
+    clientSecret: "4372a62a681b8390ac94016aa0627b0a2e30d5ea"
+};
---
+++ backend-user/config/keys-dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://alura:alura123@cluster0.vj7pq.mongodb.net/CURSOALURA?retryWrites=true&w=majority",
+    secretOrKey: "CursoAlura"
+};
